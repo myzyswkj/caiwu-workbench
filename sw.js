70f3c72cd238cc46d494c92148abeabf2dd85c02
myzyswkj <myzyswkj@163.com>
2026-08-26 11:53:22 +0800

    chore: cache v36

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  *  - 静态资源（js/css/png/svg）：stale-while-revalidate，先秒开再后台更新
  *  - 跨域请求（Supabase 等）一律放行，不进缓存
  */
-const CACHE = 'cw-cache-v35';
+const CACHE = 'cw-cache-v36';
 const PRECACHE = [
   './',
   './index.html',
